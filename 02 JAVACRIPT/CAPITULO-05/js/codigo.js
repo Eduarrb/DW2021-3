9f5e6c9bd9bd62b9ceb50432f8883a58e4e2c908
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2021-12-15 22:38:12 -0500

    spotifly final

--- 02 JAVACRIPT/CAPITULO-05/js/codigo.js
+++ 02 JAVACRIPT/CAPITULO-05/js/codigo.js
@@ -1,3 +1,22 @@
+
+
 console.log('funciona');
 
 // 1 ⚡⚡ CONCEPTOS
+// JSON -> Javascript Object Notation
+
+fetch("data/personas.json").then(respuesta => console.log(respuesta.json()));
+
+fetch("https://api.discogs.com/artists/1/releases?page=2&per_page=75").then(respuesta => console.log(respuesta.json()));
+// console.log(data);
+// fetch('archivo - URL')
+
+// ⚡⚡ EJECUCIONES SYNC
+let nombre = 'Juan';
+console.log(nombre);
+console.log('Esta es una 3ra ejecucion sincrona');
+
+// ⚡⚡ EJECUCIONES ASYNC
+setInterval(() => {
+    console.log('esta es una ejecucion asincrona');
+}, 3000);
